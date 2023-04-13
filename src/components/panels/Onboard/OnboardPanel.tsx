094f67abc53fa2f4e3a64b6735ed2d925197a34c
danillas <kaigorodov95@gmail.com>
2023-04-14 00:22:45 +0300

    onboard screen

--- src/components/panels/Onboard/OnboardPanel.tsx
+++ src/components/panels/Onboard/OnboardPanel.tsx
@@ -1,20 +1,37 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { ReactComponent as Logo } from '../../../assets/logo.svg';
 import { Button, Cell, Div, FixedLayout, Panel, Placeholder } from '@vkontakte/vkui';
 import { CommonPanelProps } from 'src/types/common';
 import { Icon28CoinsOutline, Icon28WalletOutline, Icon28StatisticsOutline } from '@vkontakte/icons';
+import { useAppContext } from 'src/components/AppContext/useAppContext';
+import './styles.scss';
 
 export function OnboardPanel(props: CommonPanelProps) {
   const { id } = props;
+
+  const { vkStorage, moves } = useAppContext();
+
+  const onClickHandler = useCallback(() => {
+    vkStorage.set('onboardEnabled', false).then(moves.resetToMainPanel);
+  }, [vkStorage, moves.resetToMainPanel]);
+
   return (
-    <Panel id={id}>
-      <Placeholder icon={<Logo />} header="Добро пожаловать!"></Placeholder>
-      <Cell before={<Icon28CoinsOutline />}>Возможность продать, отдать вещи на благотворительность</Cell>
-      <Cell before={<Icon28WalletOutline />}>Храним ваши вещи</Cell>
-      <Cell before={<Icon28StatisticsOutline />}>Эксклюзивные товары</Cell>
+    <Panel id={id} className="OnboardPanel">
+      <div className="OnboardPanel__in">
+        <Placeholder icon={<Logo />} header="Добро пожаловать!"></Placeholder>
+        <Cell before={<Icon28CoinsOutline />} multiline>
+          Продаём вещи или отдаём их на благотворительность
+        </Cell>
+        <Cell before={<Icon28WalletOutline />} multiline>
+          Храним ваши вещи
+        </Cell>
+        <Cell before={<Icon28StatisticsOutline />} multiline>
+          Предлагаем эксклюзивные товары
+        </Cell>
+      </div>
       <FixedLayout vertical="bottom">
         <Div>
-          <Button mode="primary" size="l" onClick={() => console.log('CLICK')}>
+          <Button mode="primary" size="l" onClick={onClickHandler} stretched>
             Начать
           </Button>
         </Div>
